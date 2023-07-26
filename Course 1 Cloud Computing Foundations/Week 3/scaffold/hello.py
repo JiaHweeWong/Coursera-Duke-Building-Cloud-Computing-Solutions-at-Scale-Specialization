768646912b7e990c86568dc7511c9f62a63705a7
JiaHwee <52902840+JiaHweeWong@users.noreply.github.com>
2023-07-26 22:42:06 +0800

    Update hello.py

--- Course 1 Cloud Computing Foundations/Week 3/scaffold/hello.py
+++ Course 1 Cloud Computing Foundations/Week 3/scaffold/hello.py
@@ -2,9 +2,9 @@ def add(x,y):
     return x+y
 
 
-x=1
-y=2
+a=1
+b=2
 
-result = add(x,y)
+result = add(a,b)
 
-print(f'The sum of {x} + {y} is equals to {result}')
+print(f'The sum of {a} + {b} is equals to {result}')
